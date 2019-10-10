19c76f88b50edd1a054a1db900a8c40462701add
Leonel Atencio <leonel.atencio@ucb.com>
2019-10-10 12:33:18 +0000

    Some docstring additions

--- {{cookiecutter._src_folder_name}}/ca4i_k15/connectors/{{cookiecutter.connector_name}}/src/{{cookiecutter.pipeline_stage}}/{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}.py
+++ {{cookiecutter._src_folder_name}}/ca4i_k15/connectors/{{cookiecutter.connector_name}}/src/{{cookiecutter.pipeline_stage}}/{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}.py
@@ -1,5 +1,16 @@
 from pyspark.sql import DataFrame
 
 
-def {{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}:({{cookiecutter.raw_data_abbr}}{{cookiecutter.node_name}}: DataFrame) -> DataFrame:
+def {{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}({{cookiecutter.raw_data_abbr}}{{cookiecutter.node_name}}: DataFrame) -> DataFrame:
+    """
+    This function runs the {{cookiecutter.pipeline_stage}} node for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
+
+    Args:
+        {{cookiecutter.raw_data_abbr}}{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}} (DataFrame): Raw data frame for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
+
+    Returns:
+        DataFrame: Returns the {{cookiecutter.pipeline_stage}} data frame for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
+
+    """
+
     pass
