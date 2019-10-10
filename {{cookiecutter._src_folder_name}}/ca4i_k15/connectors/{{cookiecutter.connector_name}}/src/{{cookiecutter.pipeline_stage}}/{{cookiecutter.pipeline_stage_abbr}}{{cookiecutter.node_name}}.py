b4c9d410d70f4a22bbf8c709fde755e647fd3942
Leonel Atencio <leonel.atencio@ucb.com>
2019-10-10 12:38:37 +0000

    Final fix

--- {{cookiecutter._src_folder_name}}/ca4i_k15/connectors/{{cookiecutter.connector_name}}/src/{{cookiecutter.pipeline_stage}}/{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}.py
+++ {{cookiecutter._src_folder_name}}/ca4i_k15/connectors/{{cookiecutter.connector_name}}/src/{{cookiecutter.pipeline_stage}}/{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}.py
@@ -6,7 +6,7 @@ def {{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}({{cookiecutte
     This function runs the {{cookiecutter.pipeline_stage}} node for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
 
     Args:
-        {{cookiecutter.raw_data_abbr}}{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}} (DataFrame): Raw data frame for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
+        {{cookiecutter.raw_data_abbr}}{{cookiecutter.node_name}} (DataFrame): Raw data frame for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
 
     Returns:
         DataFrame: Returns the {{cookiecutter.pipeline_stage}} data frame for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
