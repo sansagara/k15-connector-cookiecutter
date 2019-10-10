5ca34e956a36d93413a3f9623430b45b13fea59d
Leonel Atencio <leonel.atencio@ucb.com>
2019-10-10 11:46:19 +0000

    Add sources too!

--- {{cookiecutter.src_folder_name}}/ca4i_k15/connectors/{{cookiecutter.connector_name}}/src/validation/validate_{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}.py
+++ {{cookiecutter.src_folder_name}}/ca4i_k15/connectors/{{cookiecutter.connector_name}}/src/validation/validate_{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}.py
@@ -3,11 +3,11 @@ from ca4i_k15.custom_functions.validate_datasets import validate_datasets
 
 def validate_{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}({{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}, {{cookiecutter.ref_data_abbr}}{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}}):
     """
-    This function is to validate the {{pipeline_stage}} {{cookiecutter.node_name}}.
+    This function is to validate the {{cookiecutter.pipeline_stage}} {{cookiecutter.node_name}}.
 
     Args:
-        {{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}} (DataFrame): {{pipeline_stage}} dataframe for {{connector_name}} {{node_name}}.
-        {{cookiecutter.ref_data_abbr}}{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}} (DataFrame): Reference {{pipeline_stage}} dataframe for {{connector_name}} {{node_name}}
+        {{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}} (DataFrame): {{cookiecutter.pipeline_stage}} dataframe for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}.
+        {{cookiecutter.ref_data_abbr}}{{cookiecutter.pipeline_stage_abbr}}{{cookiecutter.node_name}} (DataFrame): Reference {{cookiecutter.pipeline_stage}} dataframe for {{cookiecutter.connector_name}} {{cookiecutter.node_name}}
 
     Returns:
         None
