3beaae115d8c9ec5582cbe2d125c778f55e1145d
Leonel Atencio <leonel.atencio@ucb.com>
2019-10-11 09:42:52 +0000

    Hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,23 @@
 # First get cookiecutter context dictionary:
-import cookiecutter
-from cookiecutter import prompt
+from cookiecutter.main import cookiecutter
+from cookiecutter.prompt import read_user_yes_no, read_user_variable
 
 context = {{cookiecutter}}
 
-add_more_nodes = prompt.read_user_yes_no("Want to create an additional node?", default_value='no')
+add_more_nodes = read_user_yes_no("Want to create an additional node?", default_value='no')
 if add_more_nodes:
-    new_node_name = prompt.read_user_variable("node_name", None)
+    new_node_name = read_user_variable("node_name", None)
     if not new_node_name:
         print("Skipping new node creation!")
+    else:
+        print("Creating new node!")
+        new_context = context = context.copy()
+        new_context["node_name"] = new_node_name
+        cookiecutter(
+            'k15-connector-cookiecutter',
+            extra_context=new_context,
+            no_input=True
+        )
 
     print("Context:")
     print(context)
