3687b5bd3ff0a28829567058bd9d16cd75c9e0a0
Leonel Atencio <leonel.atencio@ucb.com>
2019-10-11 12:18:52 +0000

    manipulate pipeline.yml

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,16 +1,46 @@
 # First get cookiecutter context dictionary:
 from cookiecutter.main import cookiecutter
 from cookiecutter.prompt import read_user_yes_no, read_user_variable
+import os
+import yaml
 
-context = {{cookiecutter}}
+cookiecutter_context = {{cookiecutter}}
 
-if read_user_yes_no("Should i try to append the new node to pipeline.yml?", default_value='no'):
-    import os
+
+def build_initial_pipeline_config():
+    initial_pipelines = dict()
+    initial_pipelines["{{cookiecutter.connector_name}}_{{cookiecutter.pipeline_stage}}"]["nodes"] = []
+    initial_pipelines["{{cookiecutter.connector_name}}_validation"]["nodes"] = []
+    initial_pipelines["{{cookiecutter.connector_name}}"]["pipelines"] = [
+        "{{cookiecutter.connector_name}}_{{cookiecutter.pipeline_stage}}",
+        "{{cookiecutter.connector_name}}_validation"
+    ]
+    return initial_pipelines
+
+
+def should_create_additional_nodes(context):
+    if not read_user_yes_no("Want to create an additional node?", default_value='no'):
+        print("All nodes added now!")
+        return
+
+    new_node_name = read_user_variable("node_name", None)
+    if not new_node_name:
+        print("Skipping new node creation!")
+    else:
+        print("Creating node: " + new_node_name)
+        new_context = context = context.copy()
+        new_context["node_name"] = new_node_name
+        cookiecutter(
+            'k15-connector-cookiecutter',
+            extra_context=new_context,
+            no_input=True,
+            output_dir='../',
+            overwrite_if_exists=True
+        )
+
+
+def create_or_copy_pipeline_file(pipeline_file):
     import shutil
-
-    # get pipeline directory
-    pipeline_file = os.path.join(os.getcwd(), "ca4i_k15", "connectors",
-                                 "{{cookiecutter.connector_name}}", "conf", "pipelines", "pipeline.yml")
 
     # backup pipeline.yml
     if os.path.isfile(pipeline_file):
@@ -26,46 +56,50 @@ if read_user_yes_no("Should i try to append the new node to pipeline.yml?", defa
         if not os.path.exists(os.path.dirname(pipeline_file)):
             os.makedirs(os.path.dirname(pipeline_file))
         os.mknod(pipeline_file)
+        with open(pipeline_file, "w") as f:
+            yaml.dump(build_initial_pipeline_config(), f)
 
-    # Parse pipeline.yml
-    import yaml
+
+def parse_pipeline_file(pipeline_file):
     with open(pipeline_file, 'r') as stream:
         try:
             pipelines = yaml.safe_load(stream)
             print("Parsed pipelines from pipeline.yml:" + str(pipelines))
 
-            if pipelines:
-                for pipeline in pipelines:
-                    probable_pipeline_name = "{{cookiecutter.connector_name}}" + "_" + "{{cookiecutter.pipeline_stage}}"
-                    probable_node_name = "{{cookiecutter.pipeline_stage}}" + "/" + "{{cookiecutter.pipeline_stage_abbr}}" + "{{cookiecutter.node_name}}"
-                    if not pipeline[probable_pipeline_name]:
-                        print("Created pipeline {} on pipeline.yml".format(probable_pipeline_name))
-                        pipeline[probable_pipeline_name] = []
-                    if not pipeline[probable_pipeline_name]["nodes"][probable_node_name]:
-                        print("Created node {} on pipeline {} on pipeline.yml".format(probable_node_name, probable_pipeline_name))
-                        pipeline[probable_pipeline_name]["nodes"] = pipeline[probable_pipeline_name]["nodes"].append(probable_node_name)
+            if not pipelines:
+                print("Could not parse yml on pipeline.yml")
+                return
 
-                with open(pipeline_file, "w") as f:
-                    yaml.dump(pipelines, f)
+            for pipeline in pipelines:
+                probable_pipeline_name = "{{cookiecutter.connector_name}}" + "_" + "{{cookiecutter.pipeline_stage}}"
+                probable_node_name = "{{cookiecutter.pipeline_stage}}" + "/" + "{{cookiecutter.pipeline_stage_abbr}}" + "{{cookiecutter.node_name}}"
+                if not pipeline[probable_pipeline_name]:
+                    print("Created pipeline {} on pipeline.yml".format(probable_pipeline_name))
+                    pipeline[probable_pipeline_name] = []
+                if not pipeline[probable_pipeline_name]["nodes"][probable_node_name]:
+                    print("Created node {} on pipeline {} on pipeline.yml".format(probable_node_name,
+                                                                                  probable_pipeline_name))
+                    pipeline[probable_pipeline_name]["nodes"] = pipeline[probable_pipeline_name][
+                        "nodes"].append(probable_node_name)
+
+            with open(pipeline_file, "w") as f:
+                yaml.dump(pipelines, f)
 
         except yaml.YAMLError as exc:
+            print("Exception while trying to parse pipeline.yml")
             print(exc)
 
 
-if read_user_yes_no("Want to create an additional node?", default_value='no'):
-    new_node_name = read_user_variable("node_name", None)
-    if not new_node_name:
-        print("Skipping new node creation!")
-    else:
-        print("Creating new node!")
-        new_context = context = context.copy()
-        new_context["node_name"] = new_node_name
-        cookiecutter(
-            'k15-connector-cookiecutter',
-            extra_context=new_context,
-            no_input=True,
-            output_dir='../',
-            overwrite_if_exists=True
-        )
+def should_add_to_pipeline():
+    if not read_user_yes_no("Should i try to append the new node to pipeline.yml?", default_value='no'):
+        print("Skipping node addition to pipeline.yml")
+        return
 
-print("All done!")
+    pipeline_file_path = os.path.join(os.getcwd(), "ca4i_k15", "connectors",
+                                      "{{cookiecutter.connector_name}}", "conf", "pipelines", "pipeline.yml")
+    create_or_copy_pipeline_file(pipeline_file_path)
+    parse_pipeline_file(pipeline_file_path)
+
+
+should_add_to_pipeline()
+should_create_additional_nodes(cookiecutter_context)
